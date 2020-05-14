4387ac9809b1d3a6571f306e2b4101a6f749921f
Radek Owsianko <radek112a@wp.pl>
2020-05-14 06:13:23 +0200

    to add JavaScript and finished week 4

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ console.log(firstName);
 console.log(age);
 console.log(`Czesc Nazywam sie ${firstName} i mam ${age} lat`);
 alert(`Hej jestem ${firstName} i mam ${age} lata`);
+
+const emptyParagraph = document.querySelector(".week-summary__destription--js");
+
+emptyParagraph.innerHTML = `A to jest tekst, który nagle powstał z udziałem JavaScriptu:)`;
