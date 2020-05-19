cd0ae98900284c865a5aeb4f705251dc95c27ded
Radek Owsianko <radek112a@wp.pl>
2020-05-19 06:42:27 +0200

    change colors and javascirpt welcome

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-const firstName = "Radek";
-const age = 34;
+let firstName = "Radek";
+let age = 34;
 
 console.log(firstName);
 console.log(age);
 console.log(`Czesc Nazywam sie ${firstName} i mam ${age} lat`);
-alert(`Hej jestem ${firstName} i mam ${age} lata`);
+
 
 const emptyParagraph = document.querySelector(".week-summary__destription--js");
 
 emptyParagraph.innerHTML = `A to jest tekst, który nagle powstał z udziałem JavaScriptu:)`;
+
+const welcome = (firstName,age) => {
+    alert(`Witaj Drogi odwiedzający. Mam na imię ${firstName} i mam ${age} lat.`);
+}
+welcome(`Radek`,35);
