39d66b1574227a809137a97ab3f90c13b9e8dbd0
Radek Owsianko <radek112a@wp.pl>
2020-05-31 05:51:41 +0200

    somethin with js

--- main.js
+++ main.js
@@ -11,6 +11,21 @@ const emptyParagraph = document.querySelector(".week-summary__destription--js");
 emptyParagraph.innerHTML = `A to jest tekst, który nagle powstał z udziałem JavaScriptu:)`;
 
 const welcome = (firstName,age) => {
-    alert(`Witaj Drogi odwiedzający. Mam na imię ${firstName} i mam ${age} lat.`);
+    console.log(`Witaj Drogi odwiedzający. Mam na imię ${firstName} i mam ${age} lat.`);
 }
 welcome(`Radek`,35);
+
+const result = (myNumber) => {
+    console.log(`Dostałam liczbę ${myNumber}`);
+    return myNumber*5;
+}
+
+const result_2 = result(age);
+console.log(result_2);
+
+function createContent (querySelectorContenet, content) {
+    const element = document.querySelector(querySelectorContenet);
+    element.innerHTML = content;
+}
+
+createContent(".main_header__h1--js", `Radzio Włądzio ku ku ku ka:) ha ha`);
